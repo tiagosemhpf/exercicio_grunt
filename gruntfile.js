5f3034a59eb826aa8f3a02d9fe1fd00ebe9fcb4c
Tiago Pereira <tiagopereiralopesdossantos@gmail.com>
2024-06-28 12:49:32 -0300

    first commit

---
+++ gruntfile.js
@@ -0,0 +1,22 @@
+module.exports = function(grunt) {
+    grunt.initConfig ({
+        pkg: grunt.file.readJSON('package.json'),
+        less: {
+            development: {
+                files: {
+                    'main.css': 'main.less'
+                }
+            }
+        },
+        uglify: {
+            target: {
+                files: {
+                    'dist/scripts/main.min.js': 'src/scripts/main.js'
+                }
+            }
+        }
+    })
+    grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.registerTask('default', ['less', 'uglify']);
+}
